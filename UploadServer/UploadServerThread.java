192b39a077507b774d320512909031b5c54b4e27
Sarah Liu <sarahliu1998@gmail.com>
2024-10-16 02:59:45 -0700

    uploadserver handle http POST requests -- text only atm

--- UploadServer/UploadServerThread.java
+++ UploadServer/UploadServerThread.java
@@ -5,42 +5,45 @@ import java.util.Map;
 
 public class UploadServerThread extends Thread {
    private Socket socket = null;
+
    public UploadServerThread(Socket socket) {
       super("DirServerThread");
       this.socket = socket;
    }
+
    public void run() {
       try {
          BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
          String request = in.readLine();
-         System.out.println("Request: " + request); // remove later
+         System.out.println("Request: " + request); // log to check request received
 
          Map<String, String> headers = new HashMap<>();
          String headerLine;
 
+         // FOR CHECKING HEADERS -- can delete
          while ((headerLine = in.readLine()) != null && !headerLine.isEmpty()) {
             String[] headerParts = headerLine.split(": ", 2);
             if (headerParts.length == 2) {
-               headers.put(headerParts[0], headerParts[1]);
+               headers.put(headerParts[0], headerParts[1].trim());
             }
          }
-         System.out.println("Headers: " + headers); // remove later
+         System.out.println("Headers: " + headers); // log
 
          int contentLength = 0;
          if (headers.containsKey("Content-Length")) {
             contentLength = Integer.parseInt(headers.get("Content-Length"));
          }
 
-         // remove later
+         // FOR CHECKING CONTENT/BODY -- can delete
          StringBuilder body = new StringBuilder();
          if (request.startsWith("POST") && contentLength > 0) {
             char[] bodyBuffer = new char[contentLength];
             in.read(bodyBuffer, 0, contentLength);
             body.append(bodyBuffer);
-            System.out.println("Body: " + body);
+            System.out.println("Body: " + body); // log
          }
 
-         HttpServletRequest req = new HttpServletRequest(socket.getInputStream(), headers);
+         HttpServletRequest req = new HttpServletRequest(socket.getInputStream(), headers, body.toString());
          ByteArrayOutputStream baos = new ByteArrayOutputStream();
          HttpServletResponse res = new HttpServletResponse(baos);
          HttpServlet httpServlet = new UploadServlet();
@@ -58,6 +61,8 @@ public class UploadServerThread extends Thread {
          OutputStream out = socket.getOutputStream();
          out.write(baos.toByteArray());
          socket.close();
-      } catch (Exception e) { e.printStackTrace(); }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
    }
 }
